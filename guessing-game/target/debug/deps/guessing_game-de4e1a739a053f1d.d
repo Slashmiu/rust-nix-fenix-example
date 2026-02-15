aa892031a0b871c831828462d95a55573283e4d8
Slashmiu <slash@icloud.com>
2026-02-15 12:35:06 +0900

    test

---
+++ guessing-game/target/debug/deps/guessing_game-de4e1a739a053f1d.d
@@ -0,0 +1,5 @@
+/home/slash/rust-nix-fenix-example/guessing-game/target/debug/deps/guessing_game-de4e1a739a053f1d: src/main.rs
+
+/home/slash/rust-nix-fenix-example/guessing-game/target/debug/deps/guessing_game-de4e1a739a053f1d.d: src/main.rs
+
+src/main.rs:
---
+++ guessing-game/target/debug/guessing-game.d
@@ -0,0 +1,1 @@
+/home/slash/rust-nix-fenix-example/guessing-game/target/debug/guessing-game: /home/slash/rust-nix-fenix-example/guessing-game/src/main.rs
